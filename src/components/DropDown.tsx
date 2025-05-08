e0ac9f3d53b2f163f92e14b83ed0f9bff3df1fad
mecrojun <wnsgh6588@naver.com>
2025-05-08 16:22:13 +0900

    fix & style : dropdown 컴포넌트 가려짐 현상 수정 및 base 스타일 추가

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -21,7 +21,7 @@ export function DropDownOption({ children, value, useBaseStyle, onSelect }: Drop
   return (
     <li
       value={value}
-      className={`py-[6px] text-[#6E6E82] hover:text-[#F1F1F5] hover:bg-gray-100 cursor-pointer transition ${ useBaseStyle ? 'px-[10px] rounded-lg' : '' }`}
+      className={`py-[6px] text-[#6E6E82] hover:text-[#F1F1F5] hover:bg-gray-100 cursor-pointer transition ${useBaseStyle ? 'px-[10px] rounded-lg' : ''}`}
       onClick={() => onSelect?.(value, String(children))}
     >
       {children}
@@ -29,12 +29,19 @@ export function DropDownOption({ children, value, useBaseStyle, onSelect }: Drop
   );
 }
 
-export function DropDown({ width, height, textClassName, useBaseStyle=true, children, onChange }: DropDownProps) {
+export function DropDown({
+  width,
+  height,
+  textClassName,
+  useBaseStyle = true,
+  children,
+  onChange,
+}: DropDownProps) {
   const [isOpen, setIsopen] = useState<boolean>(false);
-  const [iconSrc, setIconsrc] = useState<string>('/icon/common/dropdown.png');
+  const [iconSrc, setIconsrc] = useState<string>('/icon/icon/status=drop_down_300.png');
   const [categoryName, setCategoryName] = useState<string>('');
 
-  const baseStyle = `px-5 bg-black-400 border border-black-300 rounded-lg + ${ isOpen ? 'border-main-blue' : 'border-black-300' }`;
+  const baseStyle = `px-5 bg-black-400 border border-black-300 rounded-lg + ${isOpen ? 'border-main-blue' : 'border-black-300'}`;
 
   const dropDownRef = useRef<HTMLDivElement>(null);
 
@@ -54,13 +61,16 @@ export function DropDown({ width, height, textClassName, useBaseStyle=true, chil
       return (
         <ul
           className={`absolute top-6 left-0 z-10 flex flex-col gap-[5px] bg-[#252530] border border-[#353542] md:top-[22px] 
-            ${ useBaseStyle ? 'px-[10px] py-[10px] rounded-lg' : '' }
-            ${ height ? 'top-6 md:top-[22px]' : '' }`}
+            ${useBaseStyle ? 'px-[10px] py-[10px] rounded-lg' : ''}
+            ${height ? 'top-6 md:top-[22px]' : ''}`}
           style={{ width: width, top: height ? height : '' }}
         >
           {React.Children.map(children, (child) => {
             if (React.isValidElement<DropDownOptionProps>(child))
-              return React.cloneElement(child, { useBaseStyle: useBaseStyle, onSelect: handleSelect });
+              return React.cloneElement(child, {
+                useBaseStyle: useBaseStyle,
+                onSelect: handleSelect,
+              });
           })}
         </ul>
       );
@@ -68,10 +78,10 @@ export function DropDown({ width, height, textClassName, useBaseStyle=true, chil
 
   useEffect(() => {
     if (isOpen) {
-      setIconsrc('/icon/common/dropup.png');
+      setIconsrc('/icon/icon/status=drop_up_300.png');
       document.addEventListener('click', handleOutsideClick);
     } else {
-      setIconsrc('/icon/common/dropdown.png');
+      setIconsrc('/icon/icon/status=drop_down_300.png');
 
       if (categoryName.length === 0) {
         const childrenArray = React.Children.toArray(children);
@@ -91,15 +101,27 @@ export function DropDown({ width, height, textClassName, useBaseStyle=true, chil
   }, [isOpen, children, categoryName, handleOutsideClick]);
 
   return (
-    <div className={`relative flex items-center cursor-pointer ${useBaseStyle ? baseStyle : ''}`} style={{ width: width, height: height ? height : 'auto' }}>
+    <div
+      className={`relative flex items-center cursor-pointer ${useBaseStyle ? baseStyle : ''}`}
+      style={{ width: width, height: height ? height : 'auto' }}
+    >
       <div
         ref={dropDownRef}
         className="w-full h-6 flex justify-between items-center md:h-[22px]"
         onClick={() => setIsopen(!isOpen)}
       >
-        <span className={`${ useBaseStyle ? ( isOpen ? 'text-gray-50' : 'text-gray-200' ) : '' } ${textClassName}`}>{categoryName}</span>
+        <span
+          className={`${useBaseStyle ? (isOpen ? 'text-gray-50' : 'text-gray-200') : ''} ${textClassName}`}
+        >
+          {categoryName}
+        </span>
         <div className="relative w-6 h-6 md:w-[22px] h-[22px]">
-          <Image src={iconSrc} alt="dropIcon" fill sizes='(max-width: 767px) 24px, (min-width: 768px) 22px' />
+          <Image
+            src={iconSrc}
+            alt="dropIcon"
+            fill
+            sizes="(max-width: 767px) 24px, (min-width: 768px) 22px"
+          />
         </div>
       </div>
       <ShowDropDownOptions />
