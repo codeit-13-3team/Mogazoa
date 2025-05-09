69bb6ac9ec77c1b7fe06e4a328cd1ea7ef81143c
rover1523 <wdd152323@gmail.com>
2025-05-09 18:14:03 +0900

    Design:보더굵기 수정 rounded수정

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ label, error, ..
       <label className="text-sm font-medium text-white">{label}</label>
       <div
         className={clsx(
-          'p-[2px] rounded-md bg-transparent transition',
+          'p-[1px] rounded-md bg-transparent transition',
           !error &&
             'focus-within:bg-gradient-to-r focus-within:from-main-blue focus-within:to-main-indigo',
           error && 'bg-red',
@@ -22,7 +22,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ label, error, ..
           ref={ref}
           {...props}
           className={clsx(
-            'border rounded w-full px-3 py-2 outline-none bg-black-400 text-white',
+            'border rounded-md w-full px-2 py-2 outline-none bg-black-400 text-white',
             error ? 'border-red' : 'border-gray-500',
           )}
         />
@@ -32,5 +32,5 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ label, error, ..
   );
 });
 
-Input.displayName = 'Input'; // forwardRef
+Input.displayName = 'Input';
 export default Input;
