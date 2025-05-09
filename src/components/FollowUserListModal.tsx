d79959149dce70b2bd659480c30fa21089dee158
mecrojun <wnsgh6588@naver.com>
2025-05-09 13:02:39 +0900

    chore: 팔로우유저목록 모달 수정

--- src/components/FollowUserListModal.tsx
+++ src/components/FollowUserListModal.tsx
@@ -4,16 +4,16 @@ function showModal() {
    
 }
 
-const FolloweeUserList = () => {
+const FollowUserList = () => {
     return (
         <div className="w-full flex flex-col gap-5 lg:gap-[25px]">
-            <FolloweeUser />
-            <FolloweeUser />
+            <FollowUser />
+            <FollowUser />
         </div>
     );
 }
 
-const FolloweeUser = () => {
+const FollowUser = () => {
   return (
     <div className="h-12 flex items-center gap-5 lg:h-13">
       <div className="w-12 h-12 border border-yellow rounded-full lg:w-[52px] lg:h-[52px]"></div>
@@ -22,13 +22,13 @@ const FolloweeUser = () => {
   );
 };
 
-function FolloweeUserListModal() {
+function FollowUserListModal() {
   return (
     <div className="flex flex-col px-5 md:px-10">
       <span className="mt-10 mb-5 text-[20px] font-semibold text-gray-50 md:mt-15 md:mb-10 lg:text-[20px]">땡땡님을 팔로우하는 유저</span>
-      <FolloweeUserList />
+      <FollowUserList />
     </div>
   );
 }
 
-export default FolloweeUserListModal;
+export default FollowUserListModal;
