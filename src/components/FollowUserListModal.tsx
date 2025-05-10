7089760748d311eb3b077c6413ccc95b1810c92b
mecrojun <wnsgh6588@naver.com>
2025-05-10 15:56:27 +0900

    feat: 유저 팔로우 목록 모달 기능 완성

--- src/components/FollowUserListModal.tsx
+++ src/components/FollowUserListModal.tsx
@@ -1,33 +1,73 @@
-import Modal from "@/components/Modal";
+import Modal from '@/components/Modal';
+import { FollowUserItem } from '@/types/user';
 
-function showModal() {
-   
+import Image from 'next/image';
+
+interface FollowUserListModalProp {
+  userName: string;
+  followUserListData: FollowUserItem[];
+  onClose: () => void;
 }
 
-const FollowUserList = () => {
-    return (
-        <div className="w-full flex flex-col gap-5 lg:gap-[25px]">
-            <FollowUser />
-            <FollowUser />
-        </div>
-    );
+interface FollowUserProp {
+  nickname?: string;
+  image?: string;
 }
 
-const FollowUser = () => {
+interface FollowUserListProp {
+  followUserListData: FollowUserItem[];
+}
+
+const FollowUserList = ({ followUserListData }: FollowUserListProp) => {
+  console.log(followUserListData);
   return (
-    <div className="h-12 flex items-center gap-5 lg:h-13">
-      <div className="w-12 h-12 border border-yellow rounded-full lg:w-[52px] lg:h-[52px]"></div>
-      <span className="font-[16px] font-medium text-gray-50 lg:text-[18px]">갈릭짱짱맨</span>
+    <div className="w-full flex flex-col gap-5 lg:gap-[25px]">
+      {followUserListData ? (
+        followUserListData.map((followUser) => (
+          <FollowUser nickname={followUser.follower.nickname} image={followUser.follower.image} />
+        ))
+      ) : (
+        <div>팔로우 유저가 없습니다.</div>
+      )}
     </div>
   );
 };
 
-function FollowUserListModal() {
+const FollowUser = ({ nickname, image }: FollowUserProp) => {
   return (
-    <div className="flex flex-col px-5 md:px-10">
-      <span className="mt-10 mb-5 text-[20px] font-semibold text-gray-50 md:mt-15 md:mb-10 lg:text-[20px]">땡땡님을 팔로우하는 유저</span>
-      <FollowUserList />
+    <div className="h-12 flex items-center gap-5 lg:h-13">
+      <div className="w-12 h-12 relative border rounded-full lg:w-[52px] lg:h-[52px]">
+        {image ? (
+          <Image
+            src={image}
+            alt="유저프로필"
+            fill
+            sizes="(max-width: 1023px) 48px, (min-width: 1024px) 52px"
+          />
+        ) : (
+          <Image
+            src="/img/profileimage/profile1.png"
+            alt="유저프로필"
+            fill
+            sizes="(max-width: 1023px) 48px, (min-width: 1024px) 52px"
+          />
+        )}
+      </div>
+      <span className="font-[16px] font-medium text-gray-50 lg:text-[18px]">{nickname}</span>
     </div>
+  );
+};
+
+function FollowUserListModal({ userName, followUserListData, onClose }: FollowUserListModalProp) {
+  return (
+    <Modal onClose={onClose}>
+      <div className="flex flex-col ">
+        <span className="mb-5 text-[20px] font-semibold text-gray-50 md:mb-10 lg:text-[20px]">
+          {userName}님을 팔로우하는 유저
+        </span>
+        <FollowUserList followUserListData={followUserListData} />
+      </div>
+    </Modal>
   );
 }
 
