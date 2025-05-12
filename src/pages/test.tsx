4178437da11c8b7071a7d7e9b5a1d258f0b5926b
mecrojun <wnsgh6588@naver.com>
2025-05-12 12:57:30 +0900

    feat: 프로필 불러오는 테스트 페이지 구현

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,4 +1,23 @@
+import { getUserProfile } from "@/api/user";
+import { useEffect, useState } from "react";
+
 function test() {
+    const [profileData, setProfileData] = useState<>();
+
+    async function setProfile() {
+        const data = await getUserProfile(); 
+        setProfileData(data);
+    }
+
+    useEffect(() => { 
+        localStorage.setItem(
+        'accessToken',
+        //'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzU4LCJ0ZWFtSWQiOiIxMy0zIiwiaWF0IjoxNzQ1OTE1NDAxLCJpc3MiOiJzcC1tb2dhem9hIn0.E-JV9Vc5A-Hk3fL6iF7-D2mN5mrVUhtc0-FE7SBZ_pA',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzYxLCJ0ZWFtSWQiOiIxMy0zIiwiaWF0IjoxNzQ1OTE1MjA4LCJpc3MiOiJzcC1tb2dhem9hIn0.LI6K9y5vlvvWSKtGsSgfC-pzOAZJI3kkJUb_q-rfT8o',
+        );
+        setProfile();
+    }, []);
+
     return(
         <div></div>
     );
