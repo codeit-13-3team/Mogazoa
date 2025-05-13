39944086c8e85957bbdb724ab0e4186dacd8b84a
jihye5081 <ekftn90@gmail.com>
2025-05-13 19:54:00 +0900

    refactor: textarea 누락된 height 추가

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -20,7 +20,7 @@ function Textarea({ onChange, containerClassName, ...props }: TextareaProps) {
         value={text}
         onChange={handleChange}
         {...props}
-        className="w-full resize-none rounded-lg border border-black-300 bg-black-400 outline-none p-5 font-normal text-sm text-gray-50 plaecholder-gray-200 leading-5 mt-[10px] md:h-[160px] md:mt-[15px] md:text-base lg:leading-[22px] lg:mt-5"
+        className="w-full h-[120px] resize-none rounded-lg border border-black-300 bg-black-400 outline-none p-5 font-normal text-sm text-gray-50 plaecholder-gray-200 leading-5 mt-[10px] md:h-[160px] md:mt-[15px] md:text-base lg:leading-[22px] lg:mt-5"
       />
       <div className="absolute bottom-5 right-5 font-normal text-sm text-gray-200">
         {text.length}/500
