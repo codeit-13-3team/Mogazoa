71e7498d47adf53f87ec9f8b0e97c9982361fe1a
mecrojun <wnsgh6588@naver.com>
2025-05-13 12:24:51 +0900

    refactor: tailwind 클래스명 수정

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -12,8 +12,8 @@ interface ProfileProp {
 
 function Profile({ profileData, isMyProfile=false, editProfile, logout, onClickFollowBtn }: ProfileProp) {
   return (
-    <div className="mb-[60px] px-[20px] py-[30px] w-full h-auto rounded-[8px] bg-[#252530] md:px-[30px] lg:w-[340px] lg:mb-0">
-      <div className="w-full h-auto flex flex-col items-center gap-[30px] lg:gap-[40px]">
+    <div className="mb-[60px] px-[20px] py-[30px] w-full h-auto rounded-lg bg-black-400 md:px-[30px] lg:w-[340px] lg:mb-0">
+      <div className="w-full h-auto flex flex-col items-center gap-[30px] lg:gap-10">
         <div className="w-[120px] h-[120px] relative rounded-full lg:w-[180px] lg:h-[180px]">
           {profileData?.image ? (
             <Image src={profileData.image} alt="유저 이미지" fill />
@@ -21,27 +21,27 @@ function Profile({ profileData, isMyProfile=false, editProfile, logout, onClickF
             <Image src={noImage} alt="유저 이미지" fill />
           )}
         </div>
-        <div className="w-full flex flex-col gap-[10px] lg:gap-[20px]">
-          <span className="text-center text-[20px] font-semibold text-[#F1F1F5] lg:text-[24px]">
+        <div className="w-full flex flex-col gap-[10px] lg:gap-5">
+          <span className="text-center text-[20px] font-semibold text-gray-50 lg:text-[24px]">
             {profileData?.nickname}
           </span>
-          <p className="text-[14px] font-normal text-[#6E6E82] lg:text-[16px]">
+          <p className="text-[14px] font-normal text-gray-200 lg:text-[16px]">
             {profileData?.description}
           </p>
         </div>
         <div className="w-full px-[51px] flex justify-between relative md:px-[108px] lg:px-[58px]">
           <div className="flex flex-col items-center gap-[10px]">
-            <span className="text-[18px] font-semibold text-[#F1F1F5] lg:text-[20px]">
+            <span className="text-[18px] font-semibold text-gray-50 lg:text-[20px]">
               {profileData?.followersCount}
             </span>
-            <span className="text-[14px] font-normal text-[#9FA6B2] lg:text-[16px]">팔로워</span>
+            <span className="text-[14px] font-normal text-gray-100 lg:text-[16px]">팔로워</span>
           </div>
-          <div className="absolute left-1/2 top-0 w-px h-full bg-[#353542]"></div>
+          <div className="absolute left-1/2 top-0 w-px h-full bg-black-300"></div>
           <div className="flex flex-col items-center gap-[10px]">
-            <span className="text-[18px] font-semibold text-[#F1F1F5] lg:text-[20px]">
+            <span className="text-[18px] font-semibold text-gray-50 lg:text-[20px]">
               {profileData?.followeesCount}
             </span>
-            <span className="text-[14px] font-normal text-[#9FA6B2] lg:text-[16px]">팔로잉</span>
+            <span className="text-[14px] font-normal text-gray-100 lg:text-[16px]">팔로잉</span>
           </div>
         </div>
         {isMyProfile ? (
@@ -63,7 +63,7 @@ function Profile({ profileData, isMyProfile=false, editProfile, logout, onClickF
             </div>
           )
         ) : (
-          <div className="w-full flex flex-col gap-[10px] md:gap-[15px] lg:gap-[20px]">
+          <div className="w-full flex flex-col gap-[10px] md:gap-[15px] lg:gap-5">
             <div
               className="w-full h-[50px] flex justify-center items-center rounded-lg bg-main-blue text-gray-50 text-[16px] font-semibold hover:cursor-pointer
             md:h-[55px] lg:h-[65px] lg:text-[18px]"
