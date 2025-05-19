54ec385a2b75596fb2537a75700ad216e55cf5f1
mecrojun <wnsgh6588@naver.com>
2025-05-19 11:55:30 +0900

    chore: 콘솔로그 삭제

--- src/components/EditProfileModal.tsx
+++ src/components/EditProfileModal.tsx
@@ -1,7 +1,7 @@
 import Modal from '@/components/Modal';
-import ImageUploader from './ImageUploader';
 import { useState } from 'react';
 import Textarea from '@/components/Textarea'
+import ImageUploader from '@/components/ImageUploader';
 
 interface EditProfileModalProp {
   onClose: () => void;
@@ -11,8 +11,6 @@ function EditProfileModal({ onClose }: EditProfileModalProp) {
   const [inputText, setInputText] = useState<string>('');
   const [textarea_Text, setTextarea_Text] = useState<string>('');
 
-  console.log(textarea_Text);
-
   return (
     <Modal buttonText="저장하기" onClose={onClose}>
       <div className="flex flex-col gap-5 md:gap-10">
@@ -20,7 +18,7 @@ function EditProfileModal({ onClose }: EditProfileModalProp) {
           프로필 편집
         </span>
         <div className="flex flex-col gap-[10px] md:gap-[15px] lg:gap-[20px]">
-          <ImageUploader />
+          <ImageUploader image='' onUploadImage={() => {}} onRemoveImage={() => {}}/>
           <input
             className="w-full h-[55px] px-5 outline-none rounded-lg bg-black-400 border border-black-300 caret-white text-white text-[14px] font-normal md:h-[60px] lg:h-[70px] md:font-[16px]"
             placeholder="닉네임을 입력해 주세요"
