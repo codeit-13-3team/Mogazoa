abe1e4894a6b5ba91b646aaaa7fb59fbb292c903
juha399 <awe9224@naver.com>
2025-05-20 01:29:55 +0900

    Fix/changed color name and changed button based on state

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,6 @@ import reversedMouse from '@/assets/logo/reversedMouse.svg';
 const Error404: NextPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen px-5 text-gray-50 text-center bg-black-400 font-sans">
-       
       <div className="flex items-center gap-3 mb-3">
         <Image
           src={reversedMouse}
@@ -19,11 +18,11 @@ const Error404: NextPage = () => {
         />
         <h1 className="text-4xl md:text-8xl font-bold leading-none">404</h1>
       </div>
- 
+
       <p className="hidden md:block text-xl mb-4 drop-shadow-md leading-normal">
         Sorry, the page you are looking for could not be found.
       </p>
- 
+
       <p className="block md:hidden text-lg mb-2 drop-shadow-md leading-normal">
         Sorry, the page could not be found.
       </p> 
@@ -31,11 +30,11 @@ const Error404: NextPage = () => {
       <p className="text-base md:text-xl mb-3 drop-shadow-md leading-snug">
         죄송합니다. 페이지를 찾을 수 없습니다.
       </p>
- 
+
       <Link
         href="/"
-        className="mt-2.5 px-6 py-3 text-base font-semibold bg-white rounded-full shadow-md border 
-                   transition-all hover:scale-105 drop-shadow-sm text-black no-underline hover:text-black"
+        className="mt-2.5 px-6 py-3 text-base font-semibold bg-gray-50 rounded-full shadow-md border 
+                   transition-all hover:scale-105 hover:no-underline drop-shadow-sm text-black-500 no-underline hover:text-black-500"
       >
         홈으로 가기
       </Link>
