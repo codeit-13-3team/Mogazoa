2f395422e455ec3d2fd0f4ff5cfbab2582e47686
mecrojun <wnsgh6588@naver.com>
2025-05-24 12:53:19 +0900

    feat: 프로필 편집에서 닉네임 검사 기능 추가

--- src/components/EditProfileModal.tsx
+++ src/components/EditProfileModal.tsx
@@ -1,5 +1,5 @@
 import Modal from '@/components/Modal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Textarea from '@/components/Textarea';
 import ImageUploader from '@/components/ImageUploader';
 import Button from '@/components/button/Button';
@@ -8,11 +8,20 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { GetMeResponse } from '@/types/user';
 import { useModal } from '@/context/ModalContext';
 
+interface ErrorState {
+  isError: boolean;
+  message?: string;
+}
+
 function EditProfileModal({ profileData }: { profileData: GetMeResponse }) {
   const { closeModal } = useModal();
   const [inputText, setInputText] = useState<string>(profileData.nickname);
   const [textarea_Text, setTextarea_Text] = useState<string>(profileData.description);
   const [imageUrl, setImageUrl] = useState<string>(profileData.image);
+
+  const setInitialErrorState: ErrorState = { isError: false, message: '' };
+  const [inputError, setInputError] = useState<ErrorState>(setInitialErrorState);
+  //const isImageUrlExist: boolean = imageUrl === '' || imageUrl === 'https://none';
 
   const queryClient = useQueryClient();
 
@@ -35,6 +44,20 @@ function EditProfileModal({ profileData }: { profileData: GetMeResponse }) {
     },
   });
 
+  function checkInputError(nickname: string) {
+    if (nickname.length > 20) {
+      setInputError({ isError: true, message: '닉네임을 20자 이내로 작성해주세요.' });
+    } else if (nickname.length === 0) {
+      setInputError({ isError: true, message: '' });
+    } else {
+      setInputError(setInitialErrorState);
+    }
+  }
+
+  useEffect(() => {
+    checkInputError(inputText);
+  }, []);
+
   return (
     <Modal>
       <div className="flex flex-col gap-5 md:gap-10">
@@ -45,20 +68,31 @@ function EditProfileModal({ profileData }: { profileData: GetMeResponse }) {
             onUploadImage={(url) => setImageUrl(url)}
             onRemoveImage={() => setImageUrl('')}
           />
-          <input
-            className="w-full h-[55px] px-5 outline-none rounded-lg bg-black-400 border border-black-300 caret-white text-white text-[14px] font-normal md:h-[60px] lg:h-[70px] md:font-[16px]"
-            placeholder="닉네임을 입력해 주세요"
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-          />
+          <div>
+            <input
+              className="w-full h-[55px] px-5 outline-none rounded-lg bg-black-400 border border-black-300 caret-white text-white text-[14px] font-normal md:h-[60px] lg:h-[70px] md:font-[16px]"
+              placeholder="닉네임을 입력해 주세요"
+              value={inputText}
+              onChange={(e) => {
+                setInputText(e.target.value);
+                checkInputError(e.target.value);
+              }}
+            />
+            {inputError.isError && inputError.message ? (
+              <span className="text-xs text-red lg:text-sm">{inputError.message}</span>
+            ) : null}
+          </div>
           <Textarea
             value={textarea_Text}
-            onChange={(e) => setTextarea_Text(e.target.value)}
+            onChange={(e) => {
+              setTextarea_Text(e.target.value);
+            }}
             maxLength={300}
           />
         </div>
         <Button
           className="w-full mb-5 md:mb-8 mt-5 md:mt-10"
+          disabled={inputError.isError}
           onClick={() =>
             patchMyProfileMutation.mutate({
               description: textarea_Text,
