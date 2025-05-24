655f86ba330b666ac969018d38faefd35c41495f
jihye5081 <ekftn90@gmail.com>
2025-05-25 04:14:51 +0900

    feat: 상품 리뷰 및 수정 모달 구현 완료

--- src/api/review.ts
+++ src/api/review.ts
@@ -1,9 +1,19 @@
 import axiosInstance from './axiosInstance';
-import { CreateReviewRequest, ReviewResponse } from '@/types/review';
+import { CreateReviewRequest, ReviewResponse, UpdateReviewRequest } from '@/types/review';
 
 export const createReview = async (body: CreateReviewRequest) => {
   try {
     const response = await axiosInstance.post<ReviewResponse>('/reviews', body);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const updateReview = async (reviewId: number, body: UpdateReviewRequest) => {
+  try {
+    const response = await axiosInstance.patch<ReviewResponse>(`/reviews/${reviewId}`, body);
     return response.data;
   } catch (error) {
     console.error(error);
